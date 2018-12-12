91c839945c0dc2211e938640560590ca375070f8
Robert Perry <yapperii@gmail.com>
2018-12-12 08:35:37 -0500

    another operations (needs test)

--- src/operations/mod.rs
+++ src/operations/mod.rs
@@ -22,7 +22,9 @@ pub fn op_2NNN(mach: &mut machine::Machine, address: usize) {
     machine::jump(mach, address)
 }
 
-pub fn op_3XNN(mach: &mut machine::Machine, val: u16) {
-    //get register value
-    //compare val to register
+pub fn op_3XNN(mach: &mut machine::Machine, x: usize, n: u16) {
+    let rx = machine::get_register(mach, x) as u16;
+    if rx == n {
+        machine::increment_program_counter(mach);
+    }
 }
